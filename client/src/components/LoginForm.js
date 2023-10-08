647c2641d6a23cf61f7535534363dc001ed56e19
Quan04103 <vuviethongquan@gmail.com>
2023-10-08 23:22:08 +0700

    Test

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import logo from '../assets/logoremovebg.png'
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+const LoginForm = ({ onClose }) => {
+    return (
+        <div className="fixed inset-0 flex items-center justify-center">
+
+            <form className="bg-white max-w-screen-2xl rounded-lg shadow-lg p-4 flex flex-col space-y-4 mt-[700px] ">
+                <div class="flex min-h-full min-w-full flex-col justify-center px-6 py-12 lg:px-8">
+                    <button className="flex items-center gap-2 lg:rounded-full" onClick={onClose}>
+                        {React.createElement(XMarkIcon, { className: "h-[25px] w-[25px]" })}{" "}
+                    </button>
+                    <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
+
+                        <img class="mx-auto h-10 w-auto" src={logo} alt="Your Company" />
+                        <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Đăng nhập</h2>
+                    </div>
+                    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form class="space-y-6" action="#" method="POST">
+                            <div>
+                                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                                <div class="mt-2">
+                                    <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div class="flex items-center justify-between">
+                                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                                    <div class="text-sm">
+                                        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                                    </div>
+                                </div>
+                                <div class="mt-2">
+                                    <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
--- client/src/containers/public/Home.js
+++ client/src/containers/public/Home.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { LoginForm } from '../../components'
+import { ComplexNavbar } from './Header'
+import { useState } from 'react'
+import { LoginButton } from './Header'
+
 
 const Home = () => {
+  const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
+  console.log("isLoginPopupOpen:", isLoginPopupOpen);
+
+
   return (
-    <div>
-      <Header/>
-      <Footer/>
+    <div> 
+      <div className={isLoginPopupOpen ? 'fixed inset-0 bg-black opacity-50' : ""}>
+
+        <ComplexNavbar />
+        <Footer />
+      </div>
     </div>
 
   )
