5c38b7077a99e9c652b7e41add178929bc66e451
LeHuyLe <letantruonghuy5547@gmail.com>
2023-11-06 23:18:07 +0700

    sua 2 file [item,List]

--- client/src/components/item.js
+++ client/src/components/item.js
@@ -17,45 +17,111 @@ const Item = ({ images, user, title, star, description, attributes, address, id 
 
 
   return (
+    <div style={styles.imageContainer}>
+      <a href={`${path.DETATLS}${formatVietnameseToString(title)}/${id}`}>
+        <div style={styles.imageFrame}>
 
-    <div className="w-full h-[446px] display:flex ">
-      <div className="w-[294px] h-[446px] top-0 left-0 bg-red-100 rounded-[25px_25px_15px_15px]" >
-        <a href={`${path.DETATLS}${formatVietnameseToString(title)}/${id}`}>
-          <div className="w-[294px] h-[294px]">
-            <img className=" w-[294px] h-[294px] top-0 left-0 object-cover" src={imageroom} alt='Room' />
-          </div>
-          <div className="w-[294px] display: flex">
-            <p className=" w-[254px] h-full top-0 left-0 [font-family:'Inter-SemiBold',Helvetica] font-semibold text-black text-[16px] tracking-[0] leading-[normal]">
-              {`${address.split(',')[address.split(',').length - 2]}${address.split(',')[address.split(',').length - 1]}`}
-            </p>
-            <div className=" margin-left: 50px;  w-[16px] h-[16px]">
-              {React.createElement(StarIcon, {
-                className: `h-4 w-4 `,
-                strokeWidth: 2,
-              })}
-            </div>
-            <div class="w-10 h-[17px] text-black text-base font-semibold font-['Inter']">4.68 </div>
-          </div>
-          <div className='w-[294px] display: flex'>
-            <span className="text-black text-sm font-medium font-['Inter'] ">Diện tích: </span>
-            <span className="text-black text-sm font-normal font-['Inter']">{attributes?.acreage}</span>
-            <span className="text-black text-sm font-medium font-['Inter']"> </span>
-          </div>
-          <div class="text-black text-sm font-medium font-['Inter'] w-[294px] display: flex">Đăng tin bởi :
-            <img class="w-5 h-5 rounded-full shadow" src="https://via.placeholder.com/user" />
-            <div class="w-[140px] h-6 text-black text-sm font-medium font-['Inter']">{user?.name} </div>
-          </div>
-          <div class="w-[294px] h-[21px] text-black text-base font-semibold font-['Inter']">{attributes?.price}</div>
-        </a>
-      </div>
-
+          <img style={styles.imageroom} src={imageroom} alt={`Room `} />
+        </div>
+        <h4 style={styles.h4}>{title}</h4>
+        <pr></pr>
+        <p style={styles.p1}> {`★ ${star}`} </p>
+        <p style={styles.p2}>{`Diện tích: ${attributes?.acreage}`}</p>
+        <p style={styles.p3}>{`Đăng tin bởi: ${user?.name}`}</p>
+        <h3 style={styles.h3}>{`Giá: ${attributes?.price}`}</h3>
+      </a>
     </div>
 
   );
 };
+const styles = {
+  imageContainer: {
+    width: 'calc(20%)',
+    margin: '40px',
+  },
+  imageFrame: {
+  },
+  imageroom: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    marginBottom: '20px',
+  },
+  h4: {
+    display: 'inline',
+    fontWeight: 'bold',
+    marginBottom: '8px',
+    fontSize: '18px'
+  },
+  p1: {
+    display: 'inline',
+    margin: '20px',
+    fontWeight: 'bold',
+    marginBottom: '8px',
+    fontSize: '20px'
+  },
+  p2: {
+    marginBottom: '8px',
+    marginTop: '8px',
+  },
+  p3: {
+    marginBottom: '8px',
+  },
+  h3: {
+    fontWeight: 'bold',
+    fontSize: '20px'
+  },
+  infor: {
+    backgroundColor: 'rgba(0, 0, 0, 0.04)',
+    flex: '20%',
+  },
+  textwhy: {
+    paddingTop: '20px',
+    color: 'black',
+    fontFamily: 'Inter',
+    fontSize: '24px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 'normal',
+    textAlign: 'center',
+    marginBottom: '8px',
+  },
+  textintro: {
+    color: '#000',
+    fontfamily: 'Inter',
+    fontsize: '16px',
+    fontstyle: 'normal',
+    fontweight: '400',
+    lineheight: 'normal',
+    textAlign: 'center',
+    marginLeft: '40px',
+    marginRight: '40px',
+    marginBottom: '8px',
+  },
+  number: {
+    textAlign: 'center',
+    color: '#000',
+    fontFamily: 'Inter',
+    fontSize: '20px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 'normal',
+    marginBottom: '8px',
+  },
+  text: {
 
-
-
+    color: '#000',
+    fontfamily: 'Inter',
+    fontsize: '24px',
+    fontstyle: 'normal',
+    fontweight: '700',
+    lineheight: 'normal',
+  },
+  footer: {
+    backgroundColor: 'yellow',
+    flex: '20%',
+  },
+};
 export default memo(Item)
 
 
--- client/src/containers/public/List.js
+++ client/src/containers/public/List.js
@@ -19,27 +19,36 @@ const List = () => {
 
     return (
         <div>
+            <div style={styles.room}>
+                {posts?.length > 0 && posts.map(item => {
+                    return (
+                        <Item
+                            key={item?.id}
+                            address={item?.address}
+                            attributes={item?.attributes}
+                            description={item?.description}
+                            // images={JSON.parse(item?.images?.image)}
+                            star={+item?.star}
+                            title={item?.title}
+                            user={item?.user}
+                            id={item?.id}
 
-            {posts?.length > 0 && posts.map(item => {
-                return (
-                    <Item
-                        key={item?.id}
-                        address={item?.address}
-                        attributes={item?.attributes}
-                        description={item?.description}
-                        // images={JSON.parse(item?.images?.image)}
-                        star={+item?.star}
-                        title={item?.title}
-                        user={item?.user}
-                        id={item?.id}
-
-                    />
-                )
-            })}
+                        />
+                    )
+                })}
+            </div>
         </div>
 
     )
 }
+const styles = {
 
+    room: {
+        flex: '40%',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+};
 
 export default List
